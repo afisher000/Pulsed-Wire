6662e72f6bccdd1f7055e9fd08acab0bcc9b0403
afisher000 <43121839+afisher000@users.noreply.github.com>
2022-06-28 10:46:12 -0700

    Saved calibration signal

--- example_reading_scope.py
+++ example_reading_scope.py
@@ -14,15 +14,14 @@ import pandas as pd
 import os
 import matplotlib.pyplot as plt
 
-data_folder = '2022-06-02 (xtraj, yoffset) (2)'
-file = '(0,-1000).2.csv'
+file = 'calibration_signal'
 
 # Take scope measurement
 params = {
     'max_meas':30,
-    'channel_map':{'ch2':'x'},
+    'channel_map':{'ch1':'x', 'ch2':'y'},
     'rep_rate':1.4,
-    'filename':os.path.join(data_folder, file)
+    'filename':file
     }
 scope_id = 'USB0::0x699::0x408::C031986::INSTR'
 df = scope.read_measurements(scope_id, **params)
