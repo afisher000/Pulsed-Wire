ce74f36e58a3078f76df0999d9f250f9f6e8205d
afisher000 <43121839+afisher000@users.noreply.github.com>
2022-06-29 14:34:15 -0700

    Added Elliptical Displacement Troubleshooting

--- example_reading_scope.py
+++ example_reading_scope.py
@@ -14,14 +14,15 @@ import pandas as pd
 import os
 import matplotlib.pyplot as plt
 
-file = 'calibration_signal'
+folder = 'Elliptical Displacement Troubleshooting'
+file = '17in closer'
 
 # Take scope measurement
 params = {
     'max_meas':30,
-    'channel_map':{'ch1':'x', 'ch2':'y'},
+    'channel_map':{'ch1':'x'},
     'rep_rate':1.4,
-    'filename':file
+    'filename':os.path.join(folder, file)
     }
 scope_id = 'USB0::0x699::0x408::C031986::INSTR'
 df = scope.read_measurements(scope_id, **params)
