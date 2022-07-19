673158732412c871abfc1b3a56cc5f1750af7458
afisher000 <43121839+afisher000@users.noreply.github.com>
2022-07-18 17:18:56 -0700

    final tuning

--- example_reading_scope.py
+++ example_reading_scope.py
@@ -15,13 +15,13 @@ import os
 import matplotlib.pyplot as plt
 
 # folder = '2022-07-13 (ytraj, xoffset)'
-file = 'dispersion_0in_3pm.csv'
+file = '2022-07-18 final_trajectory_x.csv'
 
 # Take scope measurement
 params = {
     'max_meas':30,
-    'channel_map':{'ch2':'y'},
-    'rep_rate':1.0,
+    'channel_map':{'ch1':'x'},
+    'rep_rate':0.5,
     'filename':file
     }
 scope_id = 'USB0::0x699::0x408::C031986::INSTR'
