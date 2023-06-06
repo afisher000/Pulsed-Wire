0584aac6644fca52a4d35c65141eb1e596731c4d
afisher000 <43121839+afisher000@users.noreply.github.com>
2023-06-05 17:07:10 -0700

    awef

--- analysis_wirescan.py
+++ analysis_wirescan.py
@@ -35,9 +35,9 @@ archive_folder = ''
 #wirescan_folder = '2023-02-06 ytraj, xoffset'
 
 # THZ EXPERIMENT
-#wirescan_folder = '2023-05-31 xtraj, yoffset'
+# wirescan_folder = '2023-05-31 xtraj, yoffset'
 wirescan_folder = '2023-06-01 ytraj, xoffset'
-
+# 
 folder = os.path.join(archive_folder, wirescan_folder)
 #folder = wirescan_folder
 
